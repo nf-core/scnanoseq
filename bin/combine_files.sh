1cb5e268e3efcec9bcc902b1b6bc9bade64f48e5
Lara Ianov <lianov@uab.edu>
2022-11-17 15:14:40 -0600

    fixing issues with combine_files script

--- bin/combine_files.sh
+++ bin/combine_files.sh
@@ -4,8 +4,9 @@
 ### INPUT PARAMETERS ###
 ########################
 
-IN_DIR=""
+IN_DIR=`pwd`
 OUT_FILE=""
+FEATURE_TYPE=""
 
 while [[ $# -gt 0 ]]; do
     flag=$1
@@ -13,38 +14,31 @@ while [[ $# -gt 0 ]]; do
     case "${flag}" in
         -i) IN_DIR=$2; shift;;
         -o) OUT_FILE=$2; shift;;
+        -f) FEATURE_TYPE=$2; shift;;
         *) echo "Unknown option $1 ${reset}" && exit 1
     esac
 
     shift
 done
 
-OUT_DIR=$(dirname $OUT_FILE)
-
 # Print out the header
-echo -e "Sample\tFeature_Type\tEstimated_Cell_Number\tMean_Reads_Per_Cell\tMedian_Genes_Per_Cell" > ${OUT_FILE}.header.tmp
+echo -e "Sample\tFeature_Type\tEstimated_Cell_Number\tMean_Reads_Per_Cell\tMedian_Genes_Per_Cell" > .header.tmp
 
 # The files are just input from the command line
-#for in_file in $@
-for in_file in ${IN_DIR}/*.tsv
+for in_file in ${IN_DIR}/*.csv
 do
     # Parse the values from the individual files
     # Each file only has one sample, so the parsing is easier
 
     sample=$(basename $in_file | cut -f1 -d'.')
-    feature_type=$(basename $(dirname $in_file))
     values=$(tail -n1 $in_file | tr ',' '\t')
 
     # Output out the values
-    echo -e "${sample}\t${feature_type}\t${values}" >> ${OUT_DIR}/${feature_type}.data.tmp
+    echo -e "${sample}\t${FEATURE_TYPE}\t${values}" >> ${FEATURE_TYPE}.data.tmp
 done
 
 # cat the header file with the data
-for data_file in ${OUT_DIR}/*.data.tmp
-do
-    feature=$(basename $data_file | cut -f1 -d'.')
-    cat ${OUT_FILE}.header.tmp ${data_file} > ${OUT_FILE}
-done
+cat .header.tmp ${FEATURE_TYPE}.data.tmp > ${OUT_FILE}
 
 # Remove intermediary files
-rm -f ${OUT_DIR}/*.tmp
+rm -f *.tmp
